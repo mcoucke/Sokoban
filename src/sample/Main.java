bbad3bc59b957986a134ddb190f3bd13d42fa3da
BouBBoo <qbouny@gmail.com>
2018-11-29 11:27:49 +0100

    Ajout res et Génération de la grille depuis le fichier

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,9 @@ package sample;
 public class Main {
 
     public static void main(String[] args){
-        new IHMFX().lance();
+        //new IHMFX().lance();
+
+        ModeleConcret modeleConcret = new ModeleConcret();
+        modeleConcret.getGrille(3);
     }
 }
--- src/sample/ModeleConcret.java
+++ src/sample/ModeleConcret.java
@@ -1,5 +1,8 @@
 package sample;
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class ModeleConcret implements Modele {
 
     // FONCTIONS À IMPLÉMENTER
@@ -24,6 +27,59 @@ public class ModeleConcret implements Modele {
 
     public void solve(){
 
+    }
+
+    public ArrayList<String> getGrille(int niveau){
+        ArrayList<String> grille = null;
+        try {
+            File file_Micro = new File("res/MicroCosmos.txt");
+            FileReader reader = new FileReader(file_Micro);
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            grille = new ArrayList<>();
+
+            String line = bufferedReader.readLine();
+            if (niveau == 1){
+                while (line != null && !line.startsWith(";")){
+                    System.out.println(line);
+                    grille.add(line);
+                    line = String.valueOf(bufferedReader.readLine());
+                }
+
+                System.out.println(grille);
+            }
+            else {
+                int cpt_niveau = 1;
+                grille = new ArrayList<>();
+
+
+                while (line != null){
+                    if(cpt_niveau == niveau && !line.startsWith(";")){
+                        grille.add(line);
+                    }else if(line.startsWith(";") && cpt_niveau == niveau){
+                        break;
+                    }
+                    else if (line.startsWith(";")){
+                        cpt_niveau++;
+                        if(cpt_niveau == niveau){
+                            line = bufferedReader.readLine();
+                        }
+                    }
+                    line = bufferedReader.readLine();
+                }
+
+                System.out.println(grille);
+            }
+
+
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return grille;
     }
 
     @Override
