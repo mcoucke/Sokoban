3db2d82e8994ec1921769c3c33b60379046eeef0
BouBBoo <qbouny@gmail.com>
2018-12-04 09:55:52 +0100

    modif fonction getGrid
    ajout getLvl()

--- src/sample/ModeleConcret.java
+++ src/sample/ModeleConcret.java
@@ -7,78 +7,19 @@ public class ModeleConcret implements Modele {
 
     // FONCTIONS À IMPLÉMENTER
 
-    public ArrayList<String> etat = new ArrayList<String>();
+    public ArrayList<String> etat;
     public Tuple pos_perso = new Tuple();
     public ArrayList<Tuple> pos_caisses = new ArrayList<Tuple>();
+    private ArrayList<ArrayList<String> > Niveaux;
+    private int size_grid = 0;
 
 
     ModeleConcret(){
-        etat.add("#");
-        etat.add("#");
-        etat.add("#");
-        etat.add("#");
-        etat.add(" ");
-        etat.add("#");
-        etat.add("#");
-        etat.add("#");
-        etat.add("#");
-        etat.add(",");
-        etat.add("#");
-        etat.add(" ");
-        etat.add(" ");
-        etat.add("#");
-        etat.add("#");
-        etat.add("#");
-        etat.add(" ");
-        etat.add(" ");
-        etat.add("#");
-        etat.add(",");
-        etat.add("#");
-        etat.add(" ");
-        etat.add("$");
-        etat.add(" ");
-        etat.add("*");
-        etat.add(" ");
-        etat.add("$");
-        etat.add(" ");
-        etat.add("#");
-        etat.add(",");
-        etat.add("#");
-        etat.add(" ");
-        etat.add(" ");
-        etat.add(" ");
-        etat.add("+");
-        etat.add(" ");
-        etat.add(" ");
-        etat.add(" ");
-        etat.add("#");
-        etat.add(",");
-        etat.add("#");
-        etat.add("#");
-        etat.add("#");
-        etat.add(" ");
-        etat.add(".");
-        etat.add("$");
-        etat.add("#");
-        etat.add("#");
-        etat.add("#");
-        etat.add(",");
-        etat.add(" ");
-        etat.add(" ");
-        etat.add("#");
-        etat.add(" ");
-        etat.add(".");
-        etat.add(" ");
-        etat.add("#");
-        etat.add(",");
-        etat.add(" ");
-        etat.add(" ");
-        etat.add("#");
-        etat.add("#");
-        etat.add("#");
-        etat.add("#");
-        etat.add("#");
-        etat.add(",");
+        etat = new ArrayList<>();
+        Niveaux = new ArrayList<>();
+        size_grid = getSizeOfGrid(1);
+        getGrid(1);
+        System.out.println(String.valueOf(pos_perso.getX()) + " " + String.valueOf(pos_perso.getY()));
     }
 
 
@@ -134,40 +75,85 @@ public class ModeleConcret implements Modele {
             File file_Micro = new File("res/MicroCosmos.txt");
             FileReader reader = new FileReader(file_Micro);
             BufferedReader bufferedReader = new BufferedReader(reader);
-            grille = new ArrayList<>();
 
             String line = bufferedReader.readLine();
             if (niveau == 1){
+                int ligne = 0;
                 while (line != null && !line.startsWith(";")){
-                    if(line.length() < size){
-                        while (line.length() < size){
+                    if(line.length() < size_grid){
+                        while (line.length() < size_grid){
                             line += " ";
                         }
                     }
-                    line = line.replace("@", " ");
-                    line = line.replace("+", ".");
-                    line = line.replace("$", " ");
-                    line = line.replace("*", ".");
-                    System.out.println(line);
-                    grille.add(line);
+                    for (int i = 0; i < line.length(); ++i){
+                        if(line.charAt(i) == '@'){
+                            etat.add(" ");
+                            pos_perso.setPos(ligne, i);
+                        }
+                        else if(line.charAt(i) == '+'){
+                            etat.add(".");
+                            pos_perso.setPos(ligne, i);
+                        }
+                        else if(line.charAt(i) == '$'){
+                            etat.add(" ");
+                            pos_caisses.add(new Tuple(ligne, i));
+                        }
+                        else if(line.charAt(i) == '*'){
+                            etat.add(".");
+                            pos_caisses.add(new Tuple(ligne, i));
+                        }
+                        else {
+                            etat.add(String.valueOf(line.charAt(i)));
+                        }
+                    }
+                    etat.add(",");
                     line = String.valueOf(bufferedReader.readLine());
+                    ligne++;
                 }
-
-                System.out.println(grille);
             }
             else {
                 int cpt_niveau = 1;
-                grille = new ArrayList<>();
 
 
                 while (line != null){
-                    if(line.length() < size){
-                        while (line.length() < size){
+                    if(line.length() < size_grid){
+                        while (line.length() < size_grid){
                             line += " ";
                         }
                     }
                     if(cpt_niveau == niveau && !line.startsWith(";")){
-                        grille.add(line);
+                        int ligne = 0;
+                        while (line != null && !line.startsWith(";")){
+                            if(line.length() < size_grid){
+                                while (line.length() < size_grid){
+                                    line += " ";
+                                }
+                            }
+                            for (int i = 0; i < line.length(); ++i){
+                                if(line.charAt(i) == '@'){
+                                    etat.add(" ");
+                                    pos_perso.setPos(ligne, i);
+                                }
+                                else if(line.charAt(i) == '+'){
+                                    etat.add(".");
+                                    pos_perso.setPos(ligne, i);
+                                }
+                                else if(line.charAt(i) == '$'){
+                                    etat.add(" ");
+                                    pos_caisses.add(new Tuple(ligne, i));
+                                }
+                                else if(line.charAt(i) == '*'){
+                                    etat.add(".");
+                                    pos_caisses.add(new Tuple(ligne, i));
+                                }
+                                else {
+                                    etat.add(String.valueOf(line.charAt(i)));
+                                }
+                            }
+                            etat.add(",");
+                            line = String.valueOf(bufferedReader.readLine());
+                            ligne++;
+                        }
                     }else if(line.startsWith(";") && cpt_niveau == niveau){
                         break;
                     }
@@ -179,17 +165,49 @@ public class ModeleConcret implements Modele {
                     }
                     line = bufferedReader.readLine();
                 }
-
-                System.out.println(grille);
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        return grille;
     }
+
+    public void getLvl(){
+        try{
+            File file_Micro = new File("res/MicroCosmos.txt");
+            FileReader reader = new FileReader(file_Micro);
+            BufferedReader bufferedReader = new BufferedReader(reader);
+
+            String line;
+            Niveaux.add(new ArrayList<String>());
+            int current_lvl = 0;
+            int current_size = getSizeOfGrid(current_lvl + 1);
+
+            while ((line = bufferedReader.readLine()) != null){
+                if(line.contains(";")){
+                    current_lvl++;
+                    current_size = getSizeOfGrid(current_lvl + 1);
+                }else {
+                    for (int i = 0; i < line.length(); ++i){
+                        Niveaux.get(current_lvl).add(String.valueOf(line.charAt(i)));
+                    }
+                    if(line.length() < current_size){
+                        int x = line.length();
+                        while (x < current_size){
+                            Niveaux.get(current_lvl).add(" ");
+                            x++;
+                        }
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     @Override
     public void reset() {
